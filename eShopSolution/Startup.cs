814f75643425fb8180844dfa1a3efc359bb32b84
kieuminhduc01 <kieuminhduc01@gmail.com>
2020-07-03 11:36:40 +0700

    commit 1

--- eShopSolution/Startup.cs
+++ eShopSolution/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Hosting;
 namespace eShopSolution
 {
     public class Startup
-    {
+    {//dd
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
